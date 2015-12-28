e86779b9e43077ade0c7fbcbc538e44d92650739
vsukhin <vsukhin@virtuososoft.com>
2015-12-28 11:42:31 +0300

    Initial version

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"C"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// fast test
+func fastHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this request is fast")
+}
+
+// slow test
+func slowHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	fmt.Fprintf(w, "this request is slow")
+}
+
+// panic test
+func panicHandler(w http.ResponseWriter, r *http.Request) {
+	panic("something is too wrong")
+	fmt.Fprintf(w, "this request is panic")
+}
+
+func main() { application() }
+
+//export application
+func application() {
+	http.HandleFunc("/fast", fastHandler)
+	http.HandleFunc("/slow", slowHandler)
+	http.HandleFunc("/panic", panicHandler)
+
+	http.ListenAndServe(":3000", nil)
+}
